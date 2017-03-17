12544dc444b5e592c5fd6685d299afa53d52ee7c
Aaron Tweeton <aaron@aarontweeton.com>
2017-03-17 03:36:27 -0700

    Added name field.

--- index.php
+++ index.php
@@ -14,10 +14,21 @@
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-  </head>
-  <body>
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 	<script src="js/jquery-1.11.3.min.js"></script>
+  </head>
+  <body>
+	<div class="container">
+		<h1>PHP Form</h1>
+		<form method="get" enctype="multipart/form-data" action="#">
+			<div class="form-group">
+				<label for="name">Name</label>
+				<input type="text" class="form-control" id="name" placeholder="Please enter your name" required>
+			</div>
+			<button type="submit" class="btn btn-default">Submit</button>
+		</form>
+
+	</div>
 
 	<!-- Include all compiled plugins (below), or include individual files as needed --> 
 	<script src="js/bootstrap.js"></script>
